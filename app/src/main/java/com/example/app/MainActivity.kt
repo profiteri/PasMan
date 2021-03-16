c444f843f05c9fad8c2ec507ccd99670d4a7db6c
kirieiev <ge65taq@mytum.de>
2021-03-16 11:27:12 +0200

    sdaf

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val a = 1
+        val Valera = "Krasava"
     }
 }

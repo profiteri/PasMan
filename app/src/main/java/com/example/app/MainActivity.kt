99228280d81f48bec2dbb73ab00d0877a32c71a1
ge64xiz <ge64xiz@mytum.de>
2021-03-16 10:30:05 +0100

    test2

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val a = 1
+
+        val bu = findViewById<TextView>(R.id.bu)
+        bu.setOnClickListener {
+            val aniRotate = AnimationUtils.loadAnimation(applicationContext,R.anim.rotate)
+            bu.startAnimation(aniRotate)
+            val text = findViewById<TextView>(R.id.text)
+            text.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.appearance))
+            text.alpha = 0F
+        }
         val Valera = "Krasava"
+        val Serega = "Bol'noj"
     }
 }

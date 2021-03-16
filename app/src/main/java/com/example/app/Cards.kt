67a22a4d8413db7f2a39576ce0db764814f29d21
ge64xiz <ge64xiz@mytum.de>
2021-03-16 13:46:36 +0100

    цвета + мэйн с кнопкой

---
+++ app/src/main/java/com/example/app/Cards.kt
@@ -0,0 +1,23 @@
+package com.example.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+
+class Cards : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cards)
+
+        val bu = findViewById<TextView>(R.id.bu)
+        bu.setOnClickListener {
+            val aniRotate = AnimationUtils.loadAnimation(applicationContext,R.anim.rotate)
+            bu.startAnimation(aniRotate)
+            val text = findViewById<TextView>(R.id.text)
+            text.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.appearance))
+            text.alpha = 0F
+        }
+
+    }
+}
--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
@@ -10,16 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val bu = findViewById<TextView>(R.id.bu)
-        bu.setOnClickListener {
-            val aniRotate = AnimationUtils.loadAnimation(applicationContext,R.anim.rotate)
-            bu.startAnimation(aniRotate)
-            val text = findViewById<TextView>(R.id.text)
-            text.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.appearance))
-            text.alpha = 0F
-        }
-        val Valera = "Krasava"
-        val Serega = "Bol'noj"
+    fun startCards(view: View) {
+        val intent = Intent(this, Cards::class.java)
+        startActivity(intent)
+        finish()
     }
 }
